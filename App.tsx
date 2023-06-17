9b2a7e9a39abd10b5fee7ff4294d84171e11c1f4
Hayatuddeen Sadiq <ddeensadiq@yahoo.com>
2023-06-17 12:49:11 +0100

    Finish Exercise Layout App

--- App.tsx
+++ App.tsx
@@ -1,20 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Platform, StatusBar } from 'react-native';
+import Search from './src/features/Search';
+import RestaurantList from './src/features/RestaurantList';
+import { spacing } from './src/utils/sizes';
+import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.search}>
+          <Search />
+        </View>
+        <View style={styles.list}>
+          <RestaurantList />
+        </View>
+      </SafeAreaView>
+      <ExpoStatusBar style='auto' />
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: StatusBar.currentHeight,
   },
+  search: {},
+  list: {
+    flex: 1,
+  }
 });
---
+++ src/utils/colors.jsx
@@ -0,0 +1,6 @@
+export const color = {
+    white: "#ffffff",
+    black: "#000000",
+    green: 'green',
+    blue: 'blue'
+}
